902730de104e7cc142ef881ab48e4aa8b97e1e5e
Natalie-Cardot <nataliecardot@gmail.com>
2019-10-27 21:10:19 -0700

    Change folder structure, fix syntax error

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,15 @@ const browserSync = require('browser-sync').create();
 
 // Compile SCSS into CSS
 // Pre v4, would create tasks (gulp.task()), but with v4 you use functions
-function compileSCSS() {
+function style() {
   // 1. Let Gulp know where the files are
   // ** is a glob -- matches any character including a forward slash; usually used to indicate any number of subdirectories
-  return gulp.src('src/scss/**/*.scss')
+  return gulp.src('scss/**/*.scss')
     // 2. Pass that file through the compiler (uses gulp-sass)
-    .pipe(sass().on('error'), sass.logError)
+    .pipe(sass().on('error', sass.logError))
     // 3. Where do I save the compiled CSS?
     // TODO: Also should be minifying the HTML file and moving it into the build folder (currently in src)
-    .pipe(gulp.dest('build/css'))
+    .pipe(gulp.dest('css'))
     // 4. Stream changes to all browsers (ensures synchronization). Injects changes (update the CSS) without refreshing the page, keeping scroll position intact
     .pipe(browserSync.stream());
 }
@@ -23,16 +23,16 @@ function watch() {
     // Set up server
     server: {
       // Base directory
-      baseDir: 'src'
+      baseDir: './'
     }
   });
   // When anything changes, run style function defined above to recompile
-  gulp.watch('src/scss/**/*.scss', compileSCSS);
+  gulp.watch('scss/**/*.scss', style);
   // When HTML changes, trigger a page refresh
-  gulp.watch('src/**/*.html').on('change', browserSync.reload);
-  gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+  gulp.watch('./**/js/**/*.js').on('change', browserSync.reload);
 }
 
 // Exports command, allowing you to enter command to run the function with matching name defined in this file
-exports.compileSCSS = compileSCSS;
+exports.style = style;
 exports.watch = watch;
